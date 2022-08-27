b1f0e4190955a120cc8a00b33cfee8fab6d7f0d3
Geoyeob Kim <gasbank@msn.com>
2022-08-27 22:44:20 +0900

    Quit button added / Item text height adjusted

--- SMGgamejam/Assets/HyeonSu/Script/GameManager.cs
+++ SMGgamejam/Assets/HyeonSu/Script/GameManager.cs
@@ -1,28 +1,39 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField] Text TimeText;
-    [SerializeField] GameObject ClearWindow;
-    [SerializeField] GameObject UIObj;
-    [SerializeField] GameObject ClearScene;
-    [SerializeField] GameObject OverScene;
+    [SerializeField]
+    Text TimeText;
+
+    [SerializeField]
+    GameObject ClearWindow;
+
+    [SerializeField]
+    GameObject UIObj;
+
+    [SerializeField]
+    GameObject ClearScene;
+
+    [SerializeField]
+    GameObject OverScene;
 
     float timeLimit = 120;
     public float currentTime;
+
     private void Start()
     {
-        if(PlayerPrefs.GetInt("FirstStart") != 1)
+        if (PlayerPrefs.GetInt("FirstStart") != 1)
         {
             PlayerPrefs.SetInt("FirstStart", 1);
             PlayerPrefs.SetFloat("BestRecord", 100000);
         }
     }
+
     private void Update()
     {
         TimeProgress();
@@ -36,38 +47,43 @@ public class GameManager : Singleton<GameManager>
         }
 
         currentTime += Time.deltaTime;
-        
-        if(currentTime >= timeLimit)
+
+        if (currentTime >= timeLimit)
         {
             timeLimit = 100000000000000;
             GameOver();
         }
     }
+
     public void GameOver()
     {
         GameEnd(false);
         Time.timeScale = 0;
         SoundManager.Instance.PlayGameOver();
     }
+
     public void GameClear()
     {
         Instantiate(ClearWindow, UIObj.transform);
         Time.timeScale = 0;
         SoundManager.Instance.PlayGoalIn();
     }
+
     //클리어 true, 오버 false
     public void GameEnd(bool Clear)
     {
         if (Clear)
             ClearScene.SetActive(true);
         else
-             OverScene.SetActive(true);
+            OverScene.SetActive(true);
     }
+
     public void Retry()
     {
         SceneManager.LoadScene("Main");
         Time.timeScale = 1;
     }
+
     public void GoMain()
     {
         SceneManager.LoadScene("Splash");
@@ -80,18 +96,21 @@ public class GameManager : Singleton<GameManager>
         yield return new WaitForSeconds(10);
         Player.Instance.moveSpeed = 100;
     }
+
     public IEnumerator SpeedDown()
     {
         Player.Instance.moveSpeed = 80;
         yield return new WaitForSeconds(10);
         Player.Instance.moveSpeed = 100;
     }
+
     public IEnumerator UnboxingSpeedUp()
     {
         Player.Instance.unboxingProgressSpeed = 3;
         yield return new WaitForSeconds(15);
         Player.Instance.unboxingProgressSpeed = 2;
     }
+
     public IEnumerator UnboxingSpeedDown()
     {
         Player.Instance.unboxingProgressSpeed = 1;
@@ -107,5 +126,11 @@ public class GameManager : Singleton<GameManager>
         yield return new WaitForSeconds(1.5f);
         player.Boost = false;
         Debug.Log("Off");
+    }
+
+    public void QuitMain()
+    {
+        ConfirmPopup.Instance.Open("첫 화면으로 돌아가시겠습니까?", () => { SceneManager.LoadScene("Splash"); },
+            () => { ConfirmPopup.Instance.Close(); });
     }
 }
