04927d8943c6b5c11725debd8726ce60bb96c453
Geoyeob Kim <gasbank@gmail.com>
2022-08-27 17:36:39 +0900

    Add music, image license info in Credits

--- SMGgamejam/Assets/Scripts/Subcanvas.cs
+++ SMGgamejam/Assets/Scripts/Subcanvas.cs
@@ -5,15 +5,31 @@ public class Subcanvas : MonoBehaviour
     [SerializeField]
     CanvasGroup canvasGroup;
 
+    [SerializeField]
+    Animator animator;
+
+    static readonly int OpenHash = Animator.StringToHash("Open");
+    static readonly int CloseHash = Animator.StringToHash("Close");
+
     public void Open()
     {
         canvasGroup.alpha = 1;
         canvasGroup.blocksRaycasts = true;
+
+        if (animator != null)
+        {
+            animator.SetTrigger(OpenHash);
+        }
     }
 
     public void Close()
     {
         canvasGroup.alpha = 0;
         canvasGroup.blocksRaycasts = false;
+
+        if (animator != null)
+        {
+            animator.SetTrigger(CloseHash);
+        }
     }
 }
