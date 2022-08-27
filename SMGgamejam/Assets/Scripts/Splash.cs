7d75e6c2441c1efeaa7280a5763fc9d649b3ce97
Geoyeob Kim <gasbank@msn.com>
2022-08-27 12:06:58 +0900

    BGM, SFX preferences

--- SMGgamejam/Assets/Scripts/Splash.cs
+++ SMGgamejam/Assets/Scripts/Splash.cs
@@ -1,10 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Splash : MonoBehaviour
 {
+    [SerializeField]
+    AudioMixer mixer;
+
+    [SerializeField]
+    Slider bgmSlider;
+    
+    [SerializeField]
+    Slider sfxSlider;
+
+    [SerializeField]
+    AudioSource bgmSource;
+
+    void Start()
+    {
+        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 0.75f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        bgmSource.Play(500);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("HyeonTest");
@@ -13,5 +33,22 @@ public class Splash : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OnBgmSliderChange(float v)
+    {
+        mixer.SetFloat("BGMVolume", Mathf.Log10(v) * 20);
+        PlayerPrefs.SetFloat("BGMVolume", v);
+    }
+    
+    public void OnSfxSliderChange(float v)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(v) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", v);
+    }
+
+    public void SavePreferences()
+    {
+        PlayerPrefs.Save();
     }
 }
